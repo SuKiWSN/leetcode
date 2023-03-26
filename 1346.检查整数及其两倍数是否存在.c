dff9627f2c96403a95e6bd66fa3efb4e54d3706f
wanghanyu <27796554@qq.com>
2023-03-26 21:45:12 +0800

    1

--- 1346.检查整数及其两倍数是否存在.c
+++ 1346.检查整数及其两倍数是否存在.c
@@ -6,11 +6,53 @@
 
 // @lc code=start
 
+// 思路: 快排+双指针,时间复杂度O(nlogn) https://leetcode.cn/problems/check-if-n-and-its-double-exist/solutions/101742/jian-cha-zheng-shu-ji-qi-liang-bei-shu-shi-fou-cun/
 #include <stdio.h>
 #include <stdbool.h>
 
+void fastsort(int* arr, int s, int e){
+    int i = s, j = e;
+    bool f = true;
+    while(s < e){
+        if(arr[s] > arr[e]){
+            int t = arr[s];
+            arr[s] = arr[e];
+            arr[e] = t;
+            f = !f;
+        }
+        if(f)s++;
+        else e--;
+    }
+    if(i != s)fastsort(arr, i, s-1);
+    if(j != s)fastsort(arr, s+1, j);
+}
+
 bool checkIfExist(int* arr, int arrSize){
-    
+    fastsort(arr, 0, arrSize-1);
+    int p = 0, q, x, y;
+    while(p < arrSize && arr[p] < 0)p++;
+    x = p-1;
+    y = p-2;
+    q = p+1;
+    while(q<arrSize){
+        while(q<arrSize && arr[q] < arr[p] * 2)q++;
+        if(q < arrSize && arr[q] == arr[p]*2)return true;
+        p++;
+    }
+    if(q < arrSize && arr[q] == arr[p] * 2)return true;
+    while(y >=0){
+        while(y>=0 && arr[y] > arr[x] * 2)y--;
+        if(y >=0 && arr[y] == arr[x] * 2)return true;
+        x--;
+    }
+    if(y >= 0 && arr[y] == arr[x] * 2)return true;
+    return false;
 }
 // @lc code=end
 
+int main(){
+    int arr[88] = {-95,-299,357,715,-438,-759,255,418,-647,-805,-546,-182,-523,13,-79,-227,537,-655,993,-526,-518,679,-420,-53,120,187,-203,-567,-75,464,-472,-324,16,215,-773,862,-563,-839,-906,-969,633,-990,756,-17,-346,820,-216,736,188,-412,881,-599,-181,-673,802,688,553,323,-689,625,871,-938,-969,-207,-703,794,361,111,-884,156,-223,-480,-734,-838,-53,335,720,-379,855,-971,-928,99,-876,75,721,-736,-913,911};
+    bool res = checkIfExist(arr, 88);
+    printf("%d\n", res);
+    return 0;
+}
--- test.c
+++ test.c
@@ -1,14 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
+void fastsort(int* arr, int s, int e){
+    int i = s, j = e;
+    bool f = true;
+    while(s < e){
+        if(arr[s] > arr[e]){
+            int t = arr[s];
+            arr[s] = arr[e];
+            arr[e] = t;
+            f = !f;
+        }
+        if(f)s++;
+        else e--;
+    }
+    if(i != s)fastsort(arr, i, s-1);
+    if(j != s)fastsort(arr, s+1, j);
+}
 
 int main(){
-    int *p = malloc(sizeof(int)*1);
-    int *q = malloc(sizeof(int)*2);
-    *p = 10;
-    *(q+1) = 9;
-    *q = 2;
-    int **ans = malloc(sizeof(int*)*2);
-    *ans = p;
-    *(ans+1) = q;
-    printf("%d\n", *(*(ans+3)));
+    int arr[1000] = {-363,-530,423,738,-892,22,-878,605,-985,-838,216,-52,-995,999,610,305,-498,-729,457,-198,648,504,263,955,-205,-545,215,745,729,264,-434,-625,480,-554,-670,-815,813,848,879,-324,-290,-824,-751,909,644,833,139,-528,-581,-328,628,147,98,-83,623,589,445,-831,-117,170,-271,931,828,-662,-630,265,-792,-275,758,-622,407,790,895,10,250,-838,-66,-117,-553,-871,-862,363,298,-485,-328,-198,719,-246,191,-332,-119,58,271,226,333,-856,-611,829,-157,-289,78,509,18,-291};
+    int arrSize = 104;
+    fastsort(arr, 0, 87);
+    for(int i=0;i<arrSize;i++)printf("%d\n",arr[i]);
+    for(int i=0;i<arrSize;i++){
+        for(int j=i;j<arrSize;j++){
+            if(arr[i]*2 == arr[j] || arr[i] == arr[j] * 2){
+                printf("%d %d\n", arr[i], arr[j]);
+                break;
+            }
+
+        }
+    }
 }
